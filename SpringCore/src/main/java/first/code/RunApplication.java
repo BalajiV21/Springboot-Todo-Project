2d856b262428d58ce1086b49e724f10cfd9f51ad
Balaji <v.balajiviswanathan@gmail.com>
2023-02-19 12:18:50 +0530

    example project

---
+++ SpringCore/src/main/java/first/code/RunApplication.java
@@ -0,0 +1,14 @@
+package first.code;
+import java.util.*;
+
+public class RunApplication 
+{
+    private static Scanner scanner;
+
+	public static void main( String[] args )
+    {
+      
+        
+        System.out.println("github");
+    }
+}
